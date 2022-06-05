80d8271cb2f158c1272207caa85127d5186dbe01
Phil <produktivemail@googlemail.com>
2022-06-05 22:07:11 +0200

    Update 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,15 +1,16 @@
 const sumAll = function (num1, num2) {
-    let sum = 0;
+
     if (num1 > num2) {
         let temp;
         temp = num1;
         num1 = num2;
         num2 = temp;
-    } else if(num1 < 0 || num2 < 0){
+    } else if (num1 < 0 || num2 < 0) {
         return "ERROR";
-    } else if(typeof num1 !== "number" || typeof num2 !== "number"){
+    } else if (typeof num1 !== "number" || typeof num2 !== "number") {
         return "ERROR";
     }
+    let sum = 0;
     for (let i = num1; i <= num2; i++) {
         sum += i;
     }
