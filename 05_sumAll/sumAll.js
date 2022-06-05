8885ffcae76275a0f8d7b59ce50e41fe7593a12f
Phil <produktivemail@googlemail.com>
2022-06-05 21:34:18 +0200

    Add 05 solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+    let sum = 0;
+    if (num1 > num2) {
+        let temp;
+        temp = num1;
+        num1 = num2;
+        num2 = temp;
+    } else if(num1 < 0 || num2 < 0){
+        return "ERROR";
+    } else if(typeof num1 !== "number" || typeof num2 !== "number"){
+        return "ERROR";
+    }
+    for (let i = num1; i <= num2; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
