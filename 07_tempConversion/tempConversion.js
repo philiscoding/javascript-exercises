0be6d65f3749d8895bfd2e51d1dd6519177d913b
Phil <produktivemail@googlemail.com>
2022-06-05 21:59:41 +0200

    Upload 07 solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function (fahrenheit) {
+  let result = (fahrenheit - 32) * 0.555555556;
+  result = Math.round(result * 10) / 10;
+  return result;
 };
 
-const ctof = function() {
-
+const ctof = function (celcius) {
+  let result = (celcius * 1.8) + 32;
+  result = Math.round(result * 10) / 10;
+  return result;
 };
 
 // Do not edit below this line
