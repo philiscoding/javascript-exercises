af9147b01c4dec1bfa7a9b3d17679f2d1e275b66
Phil <produktivemail@googlemail.com>
2022-06-07 17:16:18 +0200

    Upload 04, didnt understand exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...otherArgs) {
+    for (let i = 0; i < array.length - 1; i++) {
+        for (let j = 0; j < otherArgs.length - 1; j++){
+            if(array[i]=== otherArgs[j]){
+                array.splice(i,1);
+            }
+        }
 
+
+
+
+    }
+    return array;
 };
 
 // Do not edit below this line
