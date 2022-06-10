4cf0781e36bffdb487fae02003528ad4e31ab2f6
Phil <produktivemail@googlemail.com>
2022-06-10 14:44:23 +0200

    Add calculator solution

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  let result = 0;
+  array.forEach(function (item) {
+    result += item;
+  });
+  return result;
+};// Array.reduce works too!
+
+const multiply = function (array) {
+  if (array.length < 2) {
+    return "Enter 2 numbers or more!";
+  }
+  let result = 1;
+  array.forEach(function (item) {
+    result *= item;
+  });
+  return result;
 };
 
-const multiply = function() {
-
+const power = function (num1, num2) {
+  return Math.pow(num1, num2);
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function (num) {
+  if(num === 0){
+    return 1;
+  }
+  let result = 1;
+  for (let i = num; i > 0; i--) {
+    result *= i;
+  }return result;
 };
 
 // Do not edit below this line
